d95c22f0e5c53e35b66ff36400b189f194390f08
Michael Tamsil <michaeltamsil@gmail.com>
2020-05-21 18:39:18 +0700

    Initial Commit

---
+++ learn-xcode-storyboard3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learn-xcode-storyboard3
+//
+//  Created by michael tamsil on 21/05/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
